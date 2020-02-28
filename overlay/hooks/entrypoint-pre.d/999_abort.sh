c11fc674af7125a28ec09ff40eef29adfdd1ff62
James Kinsman <VibroAxe@users.noreply.github.com>
2020-02-28 23:14:31 +0000

    Create 999_abort.sh

---
+++ overlay/hooks/entrypoint-pre.d/999_abort.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "You have pulled an out of date steamcache.net container"
+echo ""
+echo "Steamcache.net rebranded to LanCache.net in 2019"
+echo "The steamcache.net containers are no longer supported"
+echo ""
+echo "Please update your containers to use the new lancache.net images"
+echo ""
+echo "More details on this change can be found at https://lancache.net/news/2019/06/15/steamcache-is-rebranding-to-lancachenet/"
+echo "Or review our new documentation and guides on https://lancache.net/docs/"
